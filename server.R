63cd3097a6a73ec4af04c7e27467c61f5a794be0
“[nityakk]” <[k.nitya@gmail.com]>
2018-12-01 17:40:38 -0800

    changes to ui and server

--- server.R
+++ server.R
@@ -1,4 +1,26 @@
 library(maptools)
-data(wrld_simpl)
-myCountries = wrld_simpl@data$NAME %in% c("Australia", "United Kingdom", "Germany", "United States", "Sweden", "Netherlands", "New Zealand")
-plot(wrld_simpl, col = c(gray(.80), "red")[myCountries+1])
+library(mapproj)
+library(mapdata)
+library(ggplot2)
+library(R.utils)
+library(dplyr)
+
+my_server <- function(input, output) {
+  ##usa <- map_data("world", c("usa", "Canada"))
+  
+  ##data <- data.table::fread("data/UFOCoords.csv.bz2", stringsAsFactors=FALSE)
+  
+  output$time <- renderUI(radioButtons("time", label = h3("Time of Day"),
+                                       choices = list("AM", "PM")))
+  ##output$state <- renderUI(selectInput("state", label = h3("Pick a state"),
+  ##                                     choices = sort(unique(data$State))))
+  
+  output$map <- renderPlot({
+    if(is.null(input$time)){
+      return()
+    }
+    data(wrld_simpl)
+    myCountries = wrld_simpl@data$NAME %in% c("Australia", "United Kingdom", "Germany", "United States", "Sweden", "Netherlands", "New Zealand")
+    plot(wrld_simpl, col = c(gray(.80), "red")[myCountries+1])
+  })
+}
