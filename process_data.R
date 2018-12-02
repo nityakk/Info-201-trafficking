48c0e72e64d4da50a1a333abc42dbb773c3bc966
“[nityakk]” <[k.nitya@gmail.com]>
2018-12-01 19:57:10 -0800

    API works

--- process_data.R
+++ process_data.R
@@ -1,13 +1,13 @@
 ## setwd("~/Nitya/Au18/INFO-201/info-201-trafficking")
 
-source("~/Nitya/Au18/INFO-201/keys.R")
+source("~/Nitya/Au18/INFO-201/Info-201-trafficking/keys.R")
+
+
 library(httr)
 library(jsonlite)
 library(dplyr)
 
-base_uri <- "https://data.dol.gov/get/SweatToilAllLegalStandards/format/json"
-sweat_toil_response <- GET(base_uri,
-                add_headers("X-API-KEY" = dol.key))
+base_uri <- "https://data.dol.gov/get/SweatToilAllStatistics/limit/200"
+sweat_toil_response <- GET(url = base_uri, add_headers("X-API-KEY"= dol.key))
 sweat_toil_body <- content(sweat_toil_response, "text")
 sweat_toil_data <- fromJSON(sweat_toil_body)
-
--- ui.R
+++ ui.R
@@ -1,5 +1,9 @@
 ## add pages
 my_ui <- fluidPage(
+  tabsetPanel(
+    tabPanel("tab 1", "contents"),
+    tabPanel("tab 2", "contents"),
+    tabPanel("tab 3", "contents")),
   sidebarPanel(uiOutput("time"), uiOutput("state")),
   
   mainPanel(plotOutput("map"), textOutput("text"))
