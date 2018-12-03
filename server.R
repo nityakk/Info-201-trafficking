d1023929f3fa8fa4466e73c5fdfd8bf95878da06
“[nityakk]” <[k.nitya@gmail.com]>
2018-12-02 17:38:56 -0800

    map works

--- server.R
+++ server.R
@@ -5,24 +5,42 @@ library(ggplot2)
 library(R.utils)
 library(dplyr)
 
+source("process_data.R")
+
 my_server <- function(input, output) {
+## widgets
+  output$working_studying <- renderUI(sliderInput("working_studying",
+                             label = h3("Pecentage of Working and Studying Children"),
+                             min = 0, max = 100,
+                             sweat_toil_data$percent_of_children_working_and_studying))
+  
+## first panel
+  
+## second panel
+  
+  
+## third panel
+  
   ##usa <- map_data("world", c("usa", "Canada"))
   
   ##data <- data.table::fread("data/UFOCoords.csv.bz2", stringsAsFactors=FALSE)
-  
-  output$time <- renderUI(radioButtons("time", label = h3("Time of Day"),
-                                       choices = list("AM", "PM")))
-  ##output$state <- renderUI(selectInput("state", label = h3("Pick a state"),
-  ##                                     choices = sort(unique(data$State))))
-  
-  output$map <- renderPlot({
-    if(is.null(input$time)){
+  output$world_map <- renderPlot({
+    if(is.null(input$working_studying)){
       return()
     }
-    data(wrld_simpl)
-    myCountries = wrld_simpl@data$NAME %in% c("Australia", "United Kingdom", "Germany", "United States", "Sweden", "Netherlands", "New Zealand")
+    
+    working_studying_data <- filter(sweat_toil_data, !(percent_of_children_working_and_studying == "Unavailable"),
+                                                     !(percent_of_children_working_and_studying == "N/A")) %>%
+                             select(country, percent_of_children_working_and_studying)
+    
+    specific_percentages <- filter(working_studying_data,
+                    percent_of_children_working_and_studying >= (input$working_studying[1]/100.0),
+                    percent_of_children_working_and_studying <= (input$working_studying[2]/100.0))
+    
+    # world <- data(wrld_simpl)
+    myCountries = wrld_simpl@data$NAME %in% specific_percentages$country
     plot(wrld_simpl, col = c(gray(.80), "red")[myCountries+1])
   })
   
-  #hello
+  # fourth panel
 }
