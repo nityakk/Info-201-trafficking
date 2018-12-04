3445b0b9ccde6ad1dce1e9b08b0e0a5e974f75f8
“[nityakk]” <[k.nitya@gmail.com]>
2018-12-03 16:18:29 -0800

    clean up

--- server.R
+++ server.R
@@ -1,8 +1,5 @@
 library(maptools)
-library(mapproj)
-library(mapdata)
 library(ggplot2)
-#library(R.utils)
 library(dplyr)
 library(shiny)
 library(rsconnect)
@@ -11,27 +8,25 @@ source("process_data.R")
 
 my_server <- function(input, output) {
 ## widgets
+  ## panel 2
   output$working_studying <- renderUI(sliderInput("working_studying",
                              label = h3("Pecentage of Working and Studying Children"),
                              min = 0, max = 100,
                              sweat_toil_data$percent_of_children_working_and_studying))
+  
+  ## panel 3
   output$country <- renderUI(selectInput("country",
                     label = h3("Select a country:"), 
                     choices = data$country))
   
 ## first panel
-  # BARPLOT for question 1. Locations with highest and lowest child labor 
+  # BARPLOT: Locations with highest and lowest child labor 
   percent_work_df <- filter(sweat_toil_data, !(percent_of_working_children == "Unavailable"),
                             !(percent_of_working_children == "N/A")) %>%
     select(country, percent_of_working_children)
   
   #highest_five <- top_n(percent_work_df, 5) selects the 5 highest percentage and their countries
   #lowest_five <- top_n(percent_work_df, -5) selects the 5 lowest percentage and their countries
-  
-  # make_bars <- function(df) { #took out input, output because redundant 
-  #   result <- barplot(as.matrix(df), main = "Percentage of child labor vs. Countries",
-  #                     xlab = "Countries", ylab = "Percentage in Decimal", col = "Blue")
-  # }
   
   highest_count <- c("Chad" = 0.488, "Sierra Leone" = 0.513, "Cameroon" = 0.562, "Guinea-Bissau" = 0.574, "Somalia" = 0.995)
   lowest_count <- c("Sri Lanka" = 0.0008, "Jordan" = 0.01, "Costa Rica" = 0.011, "India" = 0.014, "Belize" = 0.016)
@@ -53,20 +48,23 @@ my_server <- function(input, output) {
   }) 
   
 ## second panel
+  ## PIE CHART: Percent child labor per area
   data <- filter(sweat_toil_data, percent_of_working_children_industry != "Unavailable")
   data <- filter(data, percent_of_working_children_industry != "N/A")
-  
-  #choices <- data$country
+
   output$piechart <- renderPlot({
     industry <- select(data, country, percent_of_working_children_industry) %>%
       filter(country == input$country) %>%
       select(percent_of_working_children_industry)
+    
     agriculture <- select(data, country, percent_of_working_children_agriculture) %>%
       filter(country == input$country) %>%
       select(percent_of_working_children_agriculture)
+    
     services <- select(data, country, percent_of_working_children_services) %>%
       filter(country == input$country) %>%
       select(percent_of_working_children_services)
+    
     slices <- c(as.double(industry), as.double(agriculture), as.double(services))
     percent <- (slices/sum(slices)*100)
     labor_slices <- c("Industry", "Agriculture", "Services")
@@ -75,6 +73,7 @@ my_server <- function(input, output) {
   })
   
 ## third panel
+  ## WORLD MAP: Percent of children working and studying
   output$world_map <- renderPlot({
     if(is.null(input$working_studying)){
       return()
@@ -94,6 +93,7 @@ my_server <- function(input, output) {
   })
   
 # fourth panel
+  ## SCATTERPLOT: Working children vs Primary completion rate
   
 }
 
